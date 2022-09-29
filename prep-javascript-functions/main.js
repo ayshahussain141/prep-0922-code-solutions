1d024c4275fd9d6b242279add14c766e4930b2c2
Aysha Hussain <ayshafajar3@gmail.com>
2022-09-29 05:47:34 +0000

    Completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,62 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function convertHoursToMinutes(x) {
+  return x * 60;
+}
+
+var minutes = convertHoursToMinutes(4);
+console.log('minutes', minutes);
+
+function getGreeting(x) {
+  return x + 'you';
+
+}
+var greeting = getGreeting('How are ');
+console.log('greeting', greeting);
+
+function addAndMultiplyBy5(x, y) {
+  return (x + y) * (5);
+}
+
+var multiply = addAndMultiplyBy5(5, 5);
+console.log('add and multiply', multiply);
+
+function multiplyAndDivideBy5(x, y) {
+  return (x * y) / (5);
+}
+
+var divide = multiplyAndDivideBy5(10, 6);
+console.log('multiply and divide', divide);
+
+function subtractTwoNumbers(x, y) {
+  return x - y;
+}
+
+var subtract = subtractTwoNumbers(15, 5);
+console.log('subtract two numbers', subtract);
+
+function getCircleCirumference(x) {
+  return (2 * 3.14) * (x);
+}
+
+var CircleCirumference = getCircleCirumference(5);
+console.log('cirle circumference', CircleCirumference);
+
+function getFullName(x, y) {
+  return x + y;
+}
+
+var fullName = getFullName('Aysha', 'Hussain');
+console.log('full name', fullName);
+
+function cube(x) {
+  return (x) * (x) * (x);
+}
+
+var cube1 = cube(5);
+console.log('cube', cube1);
